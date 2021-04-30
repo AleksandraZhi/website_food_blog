5fc55f0bf759b7f4f540f96ffe7f6faff6dd33ae
Aleksandra Ziteleva <jiterewa@gmail.com>
2021-04-30 16:03:12 +0100

    Add missing server file

---
+++ resources/controller.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const buttonWelcome = document.querySelector('.btn-welcome')
+const welcomeModalOverlay = document.querySelector('.overlay')
+const welcomeModalText = document.querySelector('.welcome')
+const galleryContainter = document.querySelector('.gallery')
+const galleryRow1 = document.querySelector('.meat-1')
+const galleryRow2 = document.querySelector('.mix')
+const galleryRow3 = document.querySelector('.meat-2')
+const galleryRow4 = document.querySelector('.cake')
+
+const request = new XMLHttpRequest()
+request.open('GET', './db/db.json', false)
+request.send(null)
+const dbObj = JSON.parse(request.responseText)
+console.log(dbObj)
+
+const imgFillRow = function (
+	container,
+	firstImageNr,
+	totalImages,
+	imgName,
+	description
+) {
+	for (let i = firstImageNr; i <= totalImages; i++) {
+		const imgNameNr = imgName + i
+		container.insertAdjacentHTML(
+			'beforeend',
+			`<div class="gallery__img"><img src="./resources/images/numbered/${imgNameNr}.jpg" alt=${description}></div>`
+		)
+	}
+}
+
+// const imgFillRowNew = function (container, firstID, numOfImgInRow) {
+// 	for (let i = firstID; i <= numOfImgInRow; i++) {
+// 		galleryContainter.insertAdjacentHTML(
+// 			'beforeend',
+// 			`<div class="gallery__img"><img src="./resources/images/numbered/${imgNameNr}.jpg" alt=${description}></div>`
+// 		)
+// 	}
+// }
+
+const screenBigLoad = function () {
+	imgFillRow(galleryRow1, 1, 6, 'salty-meat-', 'meaty dish')
+	imgFillRow(galleryRow2, 1, 6, 'salty-mix-', 'salty mixed dish')
+	imgFillRow(galleryRow3, 7, 12, 'salty-meat-', 'meaty dish')
+	imgFillRow(galleryRow4, 1, 6, 'sweet-cake-', 'cake')
+}
+
+buttonWelcome.addEventListener('click', function () {
+	welcomeModalText.classList.toggle('hidden')
+	welcomeModalOverlay.classList.toggle('hidden')
+})
+
+screenBigLoad()
